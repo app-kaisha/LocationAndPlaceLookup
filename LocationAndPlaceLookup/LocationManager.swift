080f1715cc38a61beb35ab48bbe0ffd65fb0607f
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-17 20:45:01 +0000

    Setup location and Location Manager

---
+++ LocationAndPlaceLookup/LocationManager.swift
@@ -0,0 +1,67 @@
+//
+//  LocationManager.swift
+//  LocationAndPlaceLookup
+//
+//  Created by app-kaihatsusha on 17/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+@Observable
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    // *******************************CRITICAL*******************************************
+    // Always add info.plist message for Privacy - Location When In Use Usage Description
+    
+    var location: CLLocation?
+    private let locationManager = CLLocationManager()
+    var authorisationStatus: CLAuthorizationStatus = .notDetermined
+    var errorMessage: String?
+    
+    
+    override init() {
+        super.init()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+    }
+}
+
+// Delegate Methods which Apple will call
+extension LocationManager {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let newLocation = locations.last else { return }
+        location = newLocation
+        
+        // uncomment line below if only want to get location once
+        // manager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        errorMessage = error.localizedDescription
+        print("😡🗺️ ERROR LocationManager: \(errorMessage ?? "n/a")")
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorisationStatus = manager.authorizationStatus
+        
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            print("LocationManager authorisation granted!")
+            manager.startUpdatingLocation()
+        case .denied, .restricted:
+            print("LocationManager authorisation denied!")
+            errorMessage = "😡📍 LocationManager access denied!"
+            manager.stopUpdatingLocation()
+        case .notDetermined:
+            print("LocationManager authorisation not determined!")
+            manager.requestWhenInUseAuthorization()
+        @unknown default:
+            print("Possible new enum created for CLLocationManager.authorizationStatus!s")
+            manager.requestWhenInUseAuthorization()
+        }
+    }
+}
