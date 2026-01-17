30f15b2c9682798db48471b935a02a981331d5c9
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-17 20:12:54 +0000

    Initial Commit

---
+++ LocationAndPlaceLookup/LocationAndPlaceLookupApp.swift
@@ -0,0 +1,18 @@
+//
+//  LocationAndPlaceLookupApp.swift
+//  LocationAndPlaceLookup
+//
+//  Created by app-kaihatsusha on 17/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct LocationAndPlaceLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
